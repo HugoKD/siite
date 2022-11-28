36fd40ef82686581785921e674e38679928b9e37
HugoKD <hugo.cadet@telecom-sudparis.eu>
2022-11-28 21:27:04 +0100

    patch rocket

--- bde/static/js/rocket.js
+++ bde/static/js/rocket.js
@@ -1,6 +1,6 @@
 var lastScrollTop = 0;
 $(window).scroll(function(event){
-   var st = $(this).scrollTop();
+    var st = $(this).scrollTop();
    if (st > lastScrollTop){
        $('#rocket').hide();
        $('#rocket_bottom').show();
