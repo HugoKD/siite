0cf70c9bf85474781da7ad71a2c5815f462b27f6
Adri <adri.bonnivard@gmail.com>
2023-01-05 23:39:10 +0100

    final version

--- bde/views.py
+++ bde/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from datetime import datetime
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
