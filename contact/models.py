4a82ccb4bb69170069aba3d95898f8900c474877
HugoKD <h.cadet99@gmail.com>
2023-01-30 09:11:56 +0100

    commit hugo

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
+from django import forms
 from django.db import models
 
-# Create your models here.
+class ContactModel(models.Model):
+    Prénom = models.CharField(max_length=20)
+    Nom= models.CharField(max_length=20)
+    Num= models.CharField(max_length=10)
+    Livraison= models.CharField(max_length=30)
+    Vieux= models.BooleanField()
+    Couvert= models.BooleanField()
+    Message= models.TextField(max_length=200, blank=True)
+
+
+    def __str__(self):
+        return self.Prénom + ' ' + self.Nom
