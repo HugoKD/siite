84866f6e051804899dafbc8cd8b8057a9ef566f5
HugoKD <h.cadet99@gmail.com>
2023-01-30 10:31:19 +0100

    fumulaire contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ContactModel
 
-# Register your models here.
+
+#admin.site.register(ContactModel)
+
+@admin.register(ContactModel)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('Nom', 'Mail','Objet', 'Message')
+    ordering = ('Nom',)
